f3fb913675f662b776ce49a980a0e1cc1ed39402
Benjamin Beckwith <bbeckwith@student.neumont.edu>
2018-04-12 14:18:08 -0600

    Created abstract class GUIFactory.cs

--- FactoryPattern/ClassLibrary/GUIFactory.cs
+++ FactoryPattern/ClassLibrary/GUIFactory.cs
@@ -1,12 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Windows.Controls;
+﻿using FactoryPattern;
 
 namespace ClassLibrary
 {
     abstract public class GUIFactory
     {
-        
+        abstract public void setText(Component c);
+        abstract public void setHeight(Component c);
+        abstract public void setWidth(Component c);
+        abstract public void setXPos(Component c);
+        abstract public void setYPos(Component c);
     }
 }
