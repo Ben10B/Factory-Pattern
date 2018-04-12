174d744abde95f5e1a4562ddac21cad9f9b67115
Benjamin Beckwith <bbeckwith@student.neumont.edu>
2018-04-12 14:36:52 -0600

    changed GUIFactory

---
+++ FactoryPattern/ClassLibrary/HTMLFactory.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FactoryPattern;
+
+namespace ClassLibrary
+{
+    public class HTMLFactory : GUIFactory
+    {
+        public override double setHeight(Component c)
+        {
+            return c.height;
+        }
+
+        public override string setText(Component c)
+        {
+            return c.text;
+        }
+
+        public override double setWidth(Component c)
+        {
+            return c.width;
+        }
+
+        public override double setXPos(Component c)
+        {
+            return c.xOffest;
+        }
+
+        public override double setYPos(Component c)
+        {
+            return c.yOffset;
+        }
+    }
+}
